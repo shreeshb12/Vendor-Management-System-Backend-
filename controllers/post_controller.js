2f9b5139bd453b0d254f30708be0d15384686dab
unknown <shreeshbadiger12@outlook.com>
2023-07-17 23:07:32 +0530

    delete a post and associated comments

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post=require('../models/post');
+const Comment=require('../models/comment');
 
 module.exports.create=(req,res)=>{
     Post.create({
@@ -11,4 +12,28 @@ module.exports.create=(req,res)=>{
     .catch((err)=>{
         console.log("error in creating the post"+err);
     })
+}
+
+module.exports.destroy=(req,res)=>{
+    Post.findById(req.params.id)
+    .then((post)=>{
+        console.log(post.user+" "+req.user.id);
+        if(post.user == req.user.id)
+        {
+            console.log(post.user == req.user.id);
+            post.deleteOne();
+
+            Comment.deleteMany({post:req.params.id})
+            .then((data)=>{
+                console.log('post deleted');
+            })
+            .catch((err)=>{
+                console.log(err);
+            })
+        }
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+    res.redirect('back');
 }
--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,7 @@ const passport=require('passport');
 const postController=require('../controllers/post_controller.js');
 
 router.post('/create',passport.checkAuthentication,postController.create);
+router.get('/destroy/:id',postController.destroy);
 router.use('/comments',require('./comments'));
 
 module.exports=router;
